5eae287e81b4b302ffe5a9f5be26d9c411f57320
Ayush Raut <ayushraut7558@gmail.com>
2025-09-19 14:59:56 +0530

    Explore Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 
-// Add at least one route
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/user", (req, res) => {
+    res.send({
+        firstName: "Ayush",
+        lastName: "Raut"
+    })
+})
+
+app.post("/user", (req, res) => {
+    res.send("You have succesfully updated the DB");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("You are deleting the resources!")
+})
+
+app.use("/", (req, res) => {
+    console.log("This works!");
+    res.send("Hello from Express!"); // respond to the client
 });
 
-// Start server
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+app.listen(7777);
